58b5bfef8cf6d7cec4b909e0ce3a15f7a97b64d9
LuizGalvao <ls_galvao@hotmail.com>
2026-03-04 15:05:43 -0300

    testing number of workers when using the thunder gpu cloud

--- experiments.py
+++ experiments.py
@@ -17,7 +17,7 @@ EXPERIMENTS_0 = [
         "optimizer": "SGD",  #SGD, Adam, AdamW
         "loss_function": "FocalLoss",
         "amp_enabled": True,
-        "batch_size": 64,
+        "batch_size": 32,
         "freeze_strategy": None,  #head, partial, full
         "dropout_cnn_dynamic": 0.0,
         "dropout_cnn": 0.5,
