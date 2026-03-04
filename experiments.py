bc864a726a927040f8c3b98c7a1d0cf262f1db1c
LuizGalvao <ls_galvao@hotmail.com>
2026-03-04 09:25:48 -0300

    linux exp

--- experiments.py
+++ experiments.py
@@ -12,51 +12,24 @@ EXPERIMENTS_0 = [
         "classes_type": "all_classes",  #motion_towards, all_classes, literature_classes
         "stage": 0,
         "lr_scheduler": "CosineAnnealingLRWarmUp",  #StepLR, CosineAnnealingLR, CosineAnnealingLRWarmUp
-        "global_lr": True, 
-        "optimizer": "SGD",  #SGD, Adam, AdamW
+        "global_lr": False, 
+        "optimizer": "AdamW",  #SGD, Adam, AdamW
         "loss_function": "FocalLoss",
         "amp_enabled": True,
-        "batch_size": 32,
+        "batch_size": 64,
         "freeze_strategy": None,  #head, partial, full
         "dropout_cnn_dynamic": 0.0,
         "dropout_cnn": 0.5,
         "dropout_pre_attention": 0.0,
         "dropout_fc": 0.5,
-        "backbone_lr": 1e-4,
+        "backbone_lr": 3e-5,
         "head_lr": 1e-4,
-        "weight_decay": 0.001,
+        "weight_decay": 0.01,
         "sequence_stride": 1, 
         "cached_dataset": False,
         "comments": "Testing CNN_LSTM + all_classes + CosineAnnealingLRWarmUp + Increased weight_decay"
     },
-    {
-        "input_feature_type": "single_img_input",
-        "use_object_visible_side": None,
-        "use_rear_light_status": None,
-        "model": "CNN_LSTM",
-        "input_img_type1": "img_local_context_ROI_1",
-        "input_img_type2": None,
-        "enc_input_seq_length": 16,
-        "classes_type": "all_classes",  #motion_towards, all_classes, literature_classes
-        "stage": 0,
-        "lr_scheduler": "StepLR",  #StepLR, CosineAnnealingLR, CosineAnnealingLRWarmUp
-        "global_lr": True, 
-        "optimizer": "SGD",  #SGD, Adam, AdamW
-        "loss_function": "FocalLoss",
-        "amp_enabled": True,
-        "batch_size": 32,
-        "freeze_strategy": None,  #head, partial, full
-        "dropout_cnn_dynamic": 0.0,
-        "dropout_cnn": 0.5,
-        "dropout_pre_attention": 0.0,
-        "dropout_fc": 0.5,
-        "backbone_lr": 1e-4,
-        "head_lr": 1e-4,
-        "weight_decay": 0.001,
-        "sequence_stride": 2, 
-        "cached_dataset": False,
-        "comments": "Testing CNN_LSTM + StepLR + all_classes"
-    },
+
 ]
 
 
