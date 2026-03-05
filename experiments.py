f555ff515a5477da35811ab0607ed5642b834a3f
LuizGalvao <ls_galvao@hotmail.com>
2026-03-05 17:05:37 -0300

    re testing best results fro single iamge litature classes

--- experiments.py
+++ experiments.py
@@ -34,6 +34,38 @@ EXPERIMENTS_0 = [
         "cached_dataset": False,
         "comments": "sequence_stride and backbone_lr"
     },
+    {
+        "input_feature_type": "single_img_input",
+        "use_object_visible_side": None,
+        "use_rear_light_status": None,
+        "model": "CNN_LSTM",
+        "input_img_type1": "img_local_context_ROI_1",
+        "input_img_type2": None,
+        "enc_input_seq_length": 16,
+        "classes_type": "literature_classes",  #motion_towards, all_classes, literature_classes
+        "stage": 0,
+        "run_epoch_profile": False,
+        "lr_scheduler": "CosineAnnealingLRWarmUp",  #StepLR, CosineAnnealingLR, CosineAnnealingLRWarmUp
+        "lr_cosine_t_max": 2,
+        "lr_cosine_eta_min": 1e-6,
+        "num_epochs": 30,
+        "global_lr": True, 
+        "optimizer": "SGD",  #SGD, Adam, AdamW
+        "loss_function": "FocalLoss",
+        "amp_enabled": True,
+        "batch_size": 32,
+        "freeze_strategy": None,  #head, partial, full
+        "dropout_cnn_dynamic": 0.0,
+        "dropout_cnn": 0.5,
+        "dropout_pre_attention": 0.0,
+        "dropout_fc": 0.5,
+        "backbone_lr": 2e-4,
+        "head_lr": 2e-4,
+        "weight_decay": 0.0001,
+        "sequence_stride": 4, 
+        "cached_dataset": False,
+        "comments": "sequence_stride and backbone_lr"
+    },
     
     #{
     #    "input_feature_type": "single_img_input",
