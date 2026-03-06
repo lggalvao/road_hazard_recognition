86d7efbffd3f6af623da15d8b6a1f63ae8e15438
LuizGalvao <ls_galvao@hotmail.com>
2026-03-06 17:57:48 -0300

    linux exp

--- experiments.py
+++ experiments.py
@@ -27,11 +27,11 @@ EXPERIMENTS_0 = [
         "dropout_pre_attention": 0.0,
         "dropout_fc": 0.5,
         "backbone_lr": 2e-4,
-        "head_lr": 2e-4,
-        "weight_decay": 0.00012,
+        "head_lr": 2e-5,
+        "weight_decay": 0.0001,
         "sequence_stride": 4, 
         "cached_dataset": False,
-        "comments": None
+        "comments": "sequence_stride and backbone_lr"
     },
     {
         "input_feature_type": "single_img_input",
@@ -59,107 +59,11 @@ EXPERIMENTS_0 = [
         "dropout_pre_attention": 0.0,
         "dropout_fc": 0.5,
         "backbone_lr": 2e-4,
-        "head_lr": 2e-4,
-        "weight_decay": 0.00014,
+        "head_lr": 2e-6,
+        "weight_decay": 0.0001,
         "sequence_stride": 4, 
         "cached_dataset": False,
-        "comments": None
-    },
-    {
-        "input_feature_type": "single_img_input",
-        "use_object_visible_side": None,
-        "use_rear_light_status": None,
-        "model": "CNN_LSTM",
-        "input_img_type1": "img_local_context_ROI_1",
-        "input_img_type2": None,
-        "enc_input_seq_length": 16,
-        "classes_type": "literature_classes",  #motion_towards, all_classes, literature_classes
-        "stage": 0,
-        "run_epoch_profile": False,
-        "lr_scheduler": "CosineAnnealingLRWarmUp",  #StepLR, CosineAnnealingLR, CosineAnnealingLRWarmUp
-        "lr_cosine_t_max": 1.5,
-        "lr_cosine_eta_min": 1e-6,
-        "num_epochs": 30,
-        "global_lr": True, 
-        "optimizer": "SGD",  #SGD, Adam, AdamW
-        "loss_function": "FocalLoss",
-        "amp_enabled": True,
-        "batch_size": 32,
-        "freeze_strategy": None,  #head, partial, full
-        "dropout_cnn_dynamic": 0.0,
-        "dropout_cnn": 0.5,
-        "dropout_pre_attention": 0.0,
-        "dropout_fc": 0.5,
-        "backbone_lr": 2e-4,
-        "head_lr": 2e-4,
-        "weight_decay": 0.00016,
-        "sequence_stride": 4, 
-        "cached_dataset": False,
-        "comments": None
-    },
-    {
-        "input_feature_type": "single_img_input",
-        "use_object_visible_side": None,
-        "use_rear_light_status": None,
-        "model": "CNN_LSTM",
-        "input_img_type1": "img_local_context_ROI_1",
-        "input_img_type2": None,
-        "enc_input_seq_length": 16,
-        "classes_type": "literature_classes",  #motion_towards, all_classes, literature_classes
-        "stage": 0,
-        "run_epoch_profile": False,
-        "lr_scheduler": "CosineAnnealingLRWarmUp",  #StepLR, CosineAnnealingLR, CosineAnnealingLRWarmUp
-        "lr_cosine_t_max": 1.5,
-        "lr_cosine_eta_min": 1e-6,
-        "num_epochs": 30,
-        "global_lr": True, 
-        "optimizer": "SGD",  #SGD, Adam, AdamW
-        "loss_function": "FocalLoss",
-        "amp_enabled": True,
-        "batch_size": 32,
-        "freeze_strategy": None,  #head, partial, full
-        "dropout_cnn_dynamic": 0.0,
-        "dropout_cnn": 0.5,
-        "dropout_pre_attention": 0.0,
-        "dropout_fc": 0.5,
-        "backbone_lr": 2e-4,
-        "head_lr": 2e-4,
-        "weight_decay": 0.00018,
-        "sequence_stride": 4, 
-        "cached_dataset": False,
-        "comments": None
-    },
-    {
-        "input_feature_type": "single_img_input",
-        "use_object_visible_side": None,
-        "use_rear_light_status": None,
-        "model": "CNN_LSTM",
-        "input_img_type1": "img_local_context_ROI_1",
-        "input_img_type2": None,
-        "enc_input_seq_length": 16,
-        "classes_type": "literature_classes",  #motion_towards, all_classes, literature_classes
-        "stage": 0,
-        "run_epoch_profile": False,
-        "lr_scheduler": "CosineAnnealingLRWarmUp",  #StepLR, CosineAnnealingLR, CosineAnnealingLRWarmUp
-        "lr_cosine_t_max": 1.5,
-        "lr_cosine_eta_min": 1e-6,
-        "num_epochs": 30,
-        "global_lr": True, 
-        "optimizer": "SGD",  #SGD, Adam, AdamW
-        "loss_function": "FocalLoss",
-        "amp_enabled": True,
-        "batch_size": 32,
-        "freeze_strategy": None,  #head, partial, full
-        "dropout_cnn_dynamic": 0.0,
-        "dropout_cnn": 0.5,
-        "dropout_pre_attention": 0.0,
-        "dropout_fc": 0.5,
-        "backbone_lr": 2e-4,
-        "head_lr": 2e-4,
-        "weight_decay": 0.0002,
-        "sequence_stride": 4, 
-        "cached_dataset": False,
-        "comments": None
+        "comments": "sequence_stride and backbone_lr"
     },
 
     #{
@@ -260,7 +164,7 @@ EXPERIMENTS_1 = [
         "dropout_cnn": 0.5,
         "dropout_pre_attention": 0.0,
         "dropout_fc": 0.5,
-        "backbone_lr": 3e-05,
+        "backbone_lr": 1e-06,
         "head_lr": 1e-4,
         "weight_decay": 0.01,
         "sequence_stride": 1, 
@@ -294,7 +198,7 @@ EXPERIMENTS_1 = [
         "dropout_cnn": 0.5,
         "dropout_pre_attention": 0.0,
         "dropout_fc": 0.5,
-        "backbone_lr": 3e-05,
+        "backbone_lr": 1e-07,
         "head_lr": 1e-4,
         "weight_decay": 0.01,
         "sequence_stride": 1, 
@@ -328,7 +232,7 @@ EXPERIMENTS_1 = [
         "dropout_cnn": 0.5,
         "dropout_pre_attention": 0.0,
         "dropout_fc": 0.5,
-        "backbone_lr": 3e-05,
+        "backbone_lr": 1e-08,
         "head_lr": 1e-4,
         "weight_decay": 0.01,
         "sequence_stride": 1, 
