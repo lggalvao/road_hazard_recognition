f938d3b1e7ba9fb2c9c4a25ff82a69bf7981e178
LuizGalvao <ls_galvao@hotmail.com>
2026-03-07 08:44:08 -0300

    linux exp

--- experiments.py
+++ experiments.py
@@ -69,6 +69,108 @@ EXPERIMENTS_0 = [
         "cached_dataset": False,
         "comments": "sequence_stride and backbone_lr"
     },
+    {
+        "input_feature_type": "single_img_input",
+        "use_object_visible_side": None,
+        "use_rear_light_status": None,
+        "model": "CNN_LSTM",
+        "input_img_type1": "img_local_context_ROI_1",
+        "input_img_type2": None,
+        "enc_input_seq_length": 16,
+        "classes_type": "literature_classes",  #motion_towards, all_classes, literature_classes
+        "stage": 0,
+        "run_epoch_profile": False,
+        "lr_scheduler": "CosineAnnealingLRWarmUp",  #StepLR, CosineAnnealingLR, CosineAnnealingLRWarmUp
+        "step_size": 20,
+        "gamma": 0.01,
+        "lr_cosine_t_max": 1.5,
+        "lr_cosine_eta_min": 1e-6,
+        "num_epochs": 30,
+        "global_lr": False, 
+        "optimizer": "SGD",  #SGD, Adam, AdamW
+        "loss_function": "FocalLoss",
+        "amp_enabled": False,
+        "batch_size": 32,
+        "freeze_strategy": None,  #head, partial, full
+        "dropout_cnn_dynamic": 0.0,
+        "dropout_cnn": 0.5,
+        "dropout_pre_attention": 0.0,
+        "dropout_fc": 0.5,
+        "backbone_lr": 3e-4,
+        "head_lr": 2e-6,
+        "weight_decay": 0.00018,
+        "sequence_stride": 4, 
+        "cached_dataset": False,
+        "comments": "sequence_stride and backbone_lr"
+    },
+        {
+        "input_feature_type": "single_img_input",
+        "use_object_visible_side": None,
+        "use_rear_light_status": None,
+        "model": "CNN_LSTM",
+        "input_img_type1": "img_local_context_ROI_1",
+        "input_img_type2": None,
+        "enc_input_seq_length": 16,
+        "classes_type": "literature_classes",  #motion_towards, all_classes, literature_classes
+        "stage": 0,
+        "run_epoch_profile": False,
+        "lr_scheduler": "CosineAnnealingLRWarmUp",  #StepLR, CosineAnnealingLR, CosineAnnealingLRWarmUp
+        "step_size": 20,
+        "gamma": 0.01,
+        "lr_cosine_t_max": 1.5,
+        "lr_cosine_eta_min": 1e-6,
+        "num_epochs": 30,
+        "global_lr": False, 
+        "optimizer": "SGD",  #SGD, Adam, AdamW
+        "loss_function": "FocalLoss",
+        "amp_enabled": False,
+        "batch_size": 32,
+        "freeze_strategy": None,  #head, partial, full
+        "dropout_cnn_dynamic": 0.0,
+        "dropout_cnn": 0.5,
+        "dropout_pre_attention": 0.0,
+        "dropout_fc": 0.5,
+        "backbone_lr": 4e-4,
+        "head_lr": 2e-6,
+        "weight_decay": 0.00018,
+        "sequence_stride": 4, 
+        "cached_dataset": False,
+        "comments": "sequence_stride and backbone_lr"
+    },
+        {
+        "input_feature_type": "single_img_input",
+        "use_object_visible_side": None,
+        "use_rear_light_status": None,
+        "model": "CNN_LSTM",
+        "input_img_type1": "img_local_context_ROI_1",
+        "input_img_type2": None,
+        "enc_input_seq_length": 16,
+        "classes_type": "literature_classes",  #motion_towards, all_classes, literature_classes
+        "stage": 0,
+        "run_epoch_profile": False,
+        "lr_scheduler": "CosineAnnealingLRWarmUp",  #StepLR, CosineAnnealingLR, CosineAnnealingLRWarmUp
+        "step_size": 20,
+        "gamma": 0.01,
+        "lr_cosine_t_max": 1.5,
+        "lr_cosine_eta_min": 1e-6,
+        "num_epochs": 30,
+        "global_lr": False, 
+        "optimizer": "SGD",  #SGD, Adam, AdamW
+        "loss_function": "FocalLoss",
+        "amp_enabled": False,
+        "batch_size": 32,
+        "freeze_strategy": None,  #head, partial, full
+        "dropout_cnn_dynamic": 0.0,
+        "dropout_cnn": 0.5,
+        "dropout_pre_attention": 0.0,
+        "dropout_fc": 0.5,
+        "backbone_lr": 4e-4,
+        "head_lr": 2e-5,
+        "weight_decay": 0.00018,
+        "sequence_stride": 4, 
+        "cached_dataset": False,
+        "comments": "sequence_stride and backbone_lr"
+    },
 
     #{
     #    "input_feature_type": "single_img_input",
