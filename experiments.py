542046585b0e43e85dc066d4872f8ceaf529024e
LuizGalvao <ls_galvao@hotmail.com>
2026-03-17 15:34:02 -0300

    linux exp

--- experiments.py
+++ experiments.py
@@ -1,6 +1,6 @@
 EXPERIMENTS_0 = [
     {
-        "input_feature_type": "single_img_input",
+        "input_feature_type": "single_img_input",  # explicit_feature, single_img_input, multi_img_input, explicit_and_single_img_input, explicit_and_multi_img_input
         "use_object_visible_side": None,
         "use_rear_light_status": None,
         "model": "CNN_LSTM",  # Embedding_Temporal_LSTM, Embedding_Transformer, CNN_LSTM, Embedding_CNN_LSTM, CNN_Transformer, TimeSformerNet, VideoMAENet
@@ -9,31 +9,31 @@ EXPERIMENTS_0 = [
         "enc_input_seq_length": 16,
         "enc_layers_num": 1,
         "enc_hidden_size": 128, 
-        "lstm_dropout": 0.0,
-        "classes_type": "literature_classes",  #motion_towards, all_classes, literature_classes
+        "lstm_dropout": 0.0, 
+        "classes_type": "motion_towards",  #motion_towards, all_classes, literature_classes
         "stage": 0,
         "run_epoch_profile": False,
         "lr_scheduler": "CosineAnnealingLRWarmUp",  #StepLR, CosineAnnealingLR, CosineAnnealingLRWarmUp
         "step_size": 20,
         "gamma": 0.01,
-        "lr_cosine_t_max": 1.5,
-        "lr_cosine_eta_min": 1e-6,
-        "num_epochs": 30,
-        "global_lr": False, 
-        "optimizer": "SGD",  #SGD, Adam, AdamW
+        "lr_cosine_t_max": 1,
+        "lr_cosine_eta_min": 5e-7,
+        "num_epochs": 10,
+        "global_lr": True, 
+        "optimizer": "Adam",  #SGD, Adam, AdamW
         "loss_function": "FocalLoss",
-        "focal_loss_gamma": 2,
+        "focal_loss_gamma": 3,
         "amp_enabled": True,
-        "batch_size": 32,
+        "batch_size": 16,
         "freeze_strategy": None,  #head, partial, full
-        "dropout_cnn_dynamic": 0.0,
+        "dropout_cnn_dynamic": None,
         "dropout_cnn": 0.5,
-        "dropout_pre_attention": 0.0,
-        "dropout_fc": 0.5,
-        "backbone_lr": 2e-4,
-        "head_lr": 2e-5,
-        "weight_decay": 0.00018,
-        "sequence_stride": 4, 
+        "dropout_pre_attention": None,
+        "dropout_fc": 0.6,
+        "backbone_lr": 2e-6,
+        "head_lr": None,
+        "weight_decay": 0.0015,
+        "sequence_stride": 1, 
         "cached_dataset": False,
         "comments": "best"
     },
